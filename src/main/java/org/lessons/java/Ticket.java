d7091cc359176fc9cd1447cbafd11fab38e38b29
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 15:59:26 +0100

    Add Constructor

--- src/main/java/org/lessons/java/Ticket.java
+++ src/main/java/org/lessons/java/Ticket.java
@@ -4,4 +4,14 @@ public class Ticket {
     // Attributi
     private int kmToTravel;
     private int age;
+
+    // Costruttore
+
+    public Ticket(int kmToTravel, int age) {
+        if (kmToTravel <= 0 || age <= 0) {
+            throw new IllegalArgumentException("Inserire un valore valido maggiore di 0!");
+        }
+        this.kmToTravel = kmToTravel;
+        this.age = age;
+    }
 }
