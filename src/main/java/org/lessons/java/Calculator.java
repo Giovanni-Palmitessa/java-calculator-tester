473587a97c47ff5d2096d82489f9acf12299c0fd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 15:16:21 +0100

    Fix bug of testing and add add test

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -1,38 +1,24 @@
 package org.lessons.java;
 
 public class Calculator {
-    // Attributi
-    private float num1;
-    private float num2;
-
-    //Getter e Setter
-
-    public float getNum1() {
-        return num1;
-    }
-
-    public float getNum2() {
-        return num2;
-    }
-
     //Metodi
     //Add
-    public float add(float num1, float num2){
+    public static float add(float num1, float num2){
         return num1 + num2;
     }
     //Subtract
-    public float subtract(float num1, float num2){
+    public static float subtract(float num1, float num2){
         return num1 - num2;
     }
     //Divide
-    public float divide(float num1, float num2){
+    public static float divide(float num1, float num2){
         if (num2 == 0) {
             throw new IllegalArgumentException("Il dividendo non può essere 0!");
         }
         return num1 / num2;
     }
     //Divide
-    public float multiply(float num1, float num2){
+    public static float multiply(float num1, float num2){
         return num1 * num2;
     }
 }
--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
+    @DisplayName("Test for add numbers")
     void add() {
+        float result = Calculator.add(5,3);
+        assertEquals(8.0f, result);
     }
 
     @Test
