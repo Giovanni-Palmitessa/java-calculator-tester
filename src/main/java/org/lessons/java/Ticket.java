3add7a48310bc42711f9a34ec5c227adcff0d595
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 16:04:36 +0100

    add method getTicketPrice

--- src/main/java/org/lessons/java/Ticket.java
+++ src/main/java/org/lessons/java/Ticket.java
@@ -6,12 +6,25 @@ public class Ticket {
     private int age;
 
     // Costruttore
-
     public Ticket(int kmToTravel, int age) {
         if (kmToTravel <= 0 || age <= 0) {
             throw new IllegalArgumentException("Inserire un valore valido maggiore di 0!");
         }
         this.kmToTravel = kmToTravel;
         this.age = age;
+    }
+    // Metodi
+    public double getTicketPrice() {
+        double ticketPrice = 0.21;
+        double total = kmToTravel * ticketPrice;
+        double discountJunior = 0.2;
+        double discountSenior = 0.4;
+
+        if (age < 18) {
+            total -= total * discountJunior;
+        } else if (age > 65) {
+            total -= total * discountSenior;
+        }
+        return total;
     }
 }
