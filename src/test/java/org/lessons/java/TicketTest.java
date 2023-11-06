da899820a720c115b92aef76699853db6fa1134d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 16:52:30 +0100

    first test with junior discount

--- src/test/java/org/lessons/java/TicketTest.java
+++ src/test/java/org/lessons/java/TicketTest.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class TicketTest {
 
     @Test
+    @DisplayName("Calculate Ticket price for Junior")
     void getTicketPrice() {
+        Ticket ticket = new Ticket(50, 15);
+        double price = ticket.getTicketPrice();
+        assertEquals(8.4, price, 0.01);
+        assertNotEquals(25,price,0.01);
     }
 }
