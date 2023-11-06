eca95f626a74ce4ae66714c869d98db39ea05e4e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 14:32:34 +0100

    Add attributes and getters

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -1,4 +1,17 @@
 package org.lessons.java;
 
 public class Calculator {
+    // Attributi
+    private float num1;
+    private float num2;
+
+    //Getter e Setter
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
 }
