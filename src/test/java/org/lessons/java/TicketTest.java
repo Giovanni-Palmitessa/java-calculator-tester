2e60cd538b76fa4420cd1fff264df22fb3b6e21a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 16:07:10 +0100

    add test class TicketTest

---
+++ src/test/java/org/lessons/java/TicketTest.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketTest {
+
+    @Test
+    void getTicketPrice() {
+    }
+}
