f2edb7b2fe9e73febeaad3d4af262dde7668fb3b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-06 15:25:10 +0100

    implement subtract test

--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -12,10 +12,15 @@ class CalculatorTest {
     void add() {
         float result = Calculator.add(5,3);
         assertEquals(8.0f, result);
+        assertNotEquals(11.0f, result);
     }
 
     @Test
+    @DisplayName("Test for subtract numbers")
     void subtract() {
+        float result = Calculator.subtract(10.5f, 6.5f);
+        assertEquals(4.0f, result);
+        assertNotEquals(10.5f, result);
     }
 
     @Test
